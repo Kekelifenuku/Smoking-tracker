5d9ab5e35b09993de56a9ff67791a467f27f1549
fenuku kekeli <fenuku.kekeli8989@gmail.com>
2025-08-15 16:23:16 +0000

    Initial Commit

---
+++ Smoking tracker/Smoking_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smoking_trackerApp.swift
+//  Smoking tracker
+//
+//  Created by Fenuku kekeli on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Smoking_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smoking trackerTests/Smoking_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smoking_trackerTests.swift
+//  Smoking trackerTests
+//
+//  Created by Fenuku kekeli on 8/15/25.
+//
+
+import Testing
+@testable import Smoking_tracker
+
+struct Smoking_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
